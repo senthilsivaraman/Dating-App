1b57f1e5bd6b54072342c3c3c08a8b3385ad198a
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-14 15:22:09 -0600

    SQLite - DB  created
    SQLite connected
    Migrations Created
    Async method to access DB added

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -27,12 +27,12 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("Defaultconnection")));
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             if (env.IsDevelopment())
             {
