89ba643270c7c58f0f90a6b92b516acc1d2a3f7d
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-15 00:38:26 -0600

    URL error

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -16,7 +16,7 @@ export class ValueComponent implements OnInit {
   }
 
   getValues(){
-    this.http.get('htt://localhost:5000/api/vales').subscribe(response => {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
   }, error => {
     console.log(error);
