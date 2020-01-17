7f4fbd81e685e86f2cfaa25697595dcbcb752bd6
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-16 20:29:07 -0600

    Validation for singup username

--- DatingApp.API/DTO/UserForSignUpDTO.cs
+++ DatingApp.API/DTO/UserForSignUpDTO.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTO
 {
     public class UserForSignUpDTO
     {
-         public string Name { get; set; }    
+
+        [Required]
+        public string Name { get; set; }  
+
+        [Required]
+        [StringLength(10, MinimumLength=5, ErrorMessage="Your password should be between 5-10 charcacters long")]
         public string Password { get; set; }
     }
 }
