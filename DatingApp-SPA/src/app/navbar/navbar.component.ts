f4a359faabf250f34c4d0c24be209a25ffc3b43a
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-18 12:24:10 -0600

    Injected Angular services in components (Login)
    Changed password box type to password from text, connected dotnet and angular service for cloent side loginUI

--- DatingApp-SPA/src/app/navbar/navbar.component.ts
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../_services/auth.service';
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe( next => {
+      console.log('Logged in Successfully');
+    }, error => {
+      console.log('Failed to Log in');
+    });
   }
 
 }
