e0a7e854d8e4a77bd9981c5d08a826c9a87ed4f3
senthilsivaraman <senthilkumar150393@gmail.com>
2020-01-21 23:27:53 -0600

    User class extended, users  data creation json added

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,10 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingApp.API.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } //Username
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string FullName { get; set; }    
+        public string Email { get; set; }   
+        public string Gender { get; set; }  
+        public DateTime Dob { get; set; } 
+        public DateTime AccCreated { get; set; }    
+        public DateTime LastSeen { get; set; }  
+
+        public ICollection<Photo> Photos { get; set; }
+
     }
 }
