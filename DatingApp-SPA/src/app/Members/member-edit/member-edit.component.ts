407f20752adaebd243a6d517fbcd4a2ba5a8b3f7
senthilsivaraman <senthilkumar150393@gmail.com>
2020-02-02 18:54:07 -0600

    Member Edit component Succesful from Angular, Persistent data also working

--- DatingApp-SPA/src/app/Members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/Members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -20,7 +22,7 @@ export class MemberEditComponent implements OnInit {
   }
   // Host Listner decorator has ability to listen to host(browser) and take action depands (preventing the tab close) 
   
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService, private userService: UserService, private authService: AuthService) { }
 
 
     ngOnInit() {
@@ -30,9 +32,13 @@ export class MemberEditComponent implements OnInit {
     }
 
       updateUser() {
-        console.log(this.user);
-        this.alertify.success('Profile Updated Successfully');
-        this.editForm.reset(this.user);
+        this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+          this.alertify.success('Profile Updated Successfully');
+          this.editForm.reset(this.user);
+        }, error => {
+          this.alertify.error(error);
+        });
+        
   }
 
 }
