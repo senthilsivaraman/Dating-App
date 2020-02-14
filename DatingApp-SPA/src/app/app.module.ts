8b5ef415e153cbf7f7131547d763520dc2fab941
senthilsivaraman <senthilkumar150393@gmail.com>
2020-02-14 00:10:52 -0600

    Date Format Fixed forLast seen fixed and account creat
    Using TimeAgoPipes through https://www.npmjs.com/package/time-ago-pipe
    
    Last seen correct timings are yet to be updated

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolver/member-edit-resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './Members/photo-editor/photo-editor.component';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 
 
@@ -57,7 +58,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TimeAgoPipe
   ],
   imports: [
    BrowserModule,
