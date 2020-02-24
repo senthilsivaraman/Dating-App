8a77d66f0e971bb1c6194e1aee729c131c56b6d4
senthilsivaraman <senthilkumar150393@gmail.com>
2020-02-24 00:24:03 -0600

    Retrieving list of likers and likees

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -56,6 +56,18 @@ namespace DatingApp.API.Data
 
             users = (users.Where(u => u.InterestedIn == userParams.Gender)).ToList();
 
+            if(userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = (users.Where(u => userLikers.Contains(u.Id))).ToList();
+            }
+
+            if(userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = (users.Where(u => userLikees.Contains(u.Id))).ToList();
+            }
+
             if(userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
                  var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
@@ -65,6 +77,23 @@ namespace DatingApp.API.Data
             }
             
             return users;
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(x => x.Likers)
+                .Include(x => x.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if(likers)
+            {
+                return user.Likers.Where(user => user.LikeeId == id).Select(i => i.LikerId);
+            }
+
+            else {
+                return user.Likees.Where(user => user.LikerId == id).Select(i => i.LikeeId);
+            }
         }
 
         public async Task<bool> SaveAll()
--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -7,5 +7,9 @@ namespace DatingApp.API.Helpers
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
+
+        public bool Likees { get; set; } = false;
+        public bool Likers { get; set; } = false;
+        
     }
 }
