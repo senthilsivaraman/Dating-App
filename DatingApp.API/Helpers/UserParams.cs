b2f4c37ed51df7bb15d622700e51ff43272c2f91
senthilsivaraman <senthilkumar150393@gmail.com>
2020-03-01 22:39:03 -0600

    Pagination Implementation
    End of Sec 141

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -2,6 +2,15 @@ namespace DatingApp.API.Helpers
 {
     public class UserParams
     {
+        private const int MaxPageSize = 50;
+        private int PageNumber { get; set; } = 1; 
+        private int pageSize = 10;  
+        private int PageSize
+        {
+            get {return pageSize;}
+            set {pageSize = (value > MaxPageSize) ? MaxPageSize : value ;}
+        } 
+         
         public int UserId { get; set; } 
         public string InterestedIn { get; set; }
         public string Gender { get; set; }
